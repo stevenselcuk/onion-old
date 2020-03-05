f7ae9f4723910d5001c2724d04a6fdf43062b318
Steven J. Selcuk <stevenjselcuk@gmail.com>
2020-03-05 21:33:57 +0300

    New Deps:  @babel/plugin-transform-runtime has added

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,6 @@
 const resolve = m => require.resolve(m)
+const IS_TEST = (process.env.BABEL_ENV || process.env.NODE_ENV) === `test`
+const IS_PROD = (process.env.BABEL_ENV || process.env.NODE_ENV) === `production`
 
 module.exports = {
   presets: ['@babel/preset-env', '@babel/preset-react'],
@@ -7,6 +9,13 @@ module.exports = {
       resolve(`@babel/plugin-proposal-class-properties`),
       {
         loose: true,
+      },
+    ],
+    resolve(`@babel/plugin-transform-runtime`),
+    [
+      resolve(`@babel/plugin-transform-spread`),
+      {
+        loose: false,
       },
     ],
   ]
